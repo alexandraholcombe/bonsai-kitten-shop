4c7c551e0f18fb5563d6fbe942af8c056d412271
Alexandra Holcombe and Chad Durkin <alexandra@>
2017-01-11 17:22:59 -0800

    Move variable declarations outside of submit scope

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 $(document).ready(function() {
+  var numOfSanitizer = $("select#sanitizer").val();
+  var numOfFormula = $("select#formula").val();
+  var numOfPickled = $("select#pickled").val();
+  var numOfCollector = $("select#collector").val();
+  var numOfKittenJr = $("select#kittenJr").val();
+  var numOfKandle = $("select#kandle").val();
+
+  //Will moving these variables out change when the values get assigned?
+
   $("form").submit(function(event) {
-
       event.preventDefault();
-      var numOfSanitizer = $("select#sanitizer").val();
-      var numOfFormula = $("select#formula").val();
-      var numOfPickled = $("select#pickled").val();
-      var numOfCollector = $("select#collector").val();
-      var numOfKittenJr = $("select#kittenJr").val();
-      var numOfKandle = $("select#kandle").val();
-
-      $("#output1").toggle();
       $("#output1").text(numOfSanitizer);
       $("#output2").text(numOfFormula);
       $("#output3").text(numOfPickled);
       $("#output4").text(numOfCollector);
       $("#output5").text(numOfKittenJr);
       $("#output6").text(numOfKandle);
+  });
+
+  $("form").submit(function() {
+    $("#output1").show();
   });
 
 });
